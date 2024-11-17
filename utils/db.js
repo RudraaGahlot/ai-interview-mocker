7026a7853754ac4a2849d041eb853e2a431228d7
RudraaGahlot <115854937+RudraaGahlot@users.noreply.github.com>
2024-11-17 20:37:07 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,5 +5,5 @@ import * as schema from "./schema"
 
 config({ path: ".env" }); // or .env.local
 
-const sql = neon("postgresql://neondb_owner:TJ63PDXMlOCA@ep-divine-snow-a5airy6w.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require");
+const sql = neon(DATABASE_URl);
 export const db = drizzle(sql, {schema} );
